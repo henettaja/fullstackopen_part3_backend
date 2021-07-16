6ae54ace77c72fc88ca2ac31612216f9c7f92372
henettaja <henri.vaisane@gmail.com>
2021-07-16 17:06:35 +0300

    Ready for Heroku

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require("express")
 const morgan = require("morgan")
+const cors = require("cors")
 const app = express()
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.use(express.json())
 app.use(morgan(":method :url :status :res[content-length] :data /// :response-time ms"))
+app.use(cors())
 
 morgan.token("data", (req, res) => {
 	if (req.method === "POST") return JSON.stringify(req.body)
